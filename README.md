07130fc192567e04478c51b2a236e92d0eb0c476
Gulshan Kumar Prasad <gulshankumarprasad04@gmail.com>
2026-01-16 22:29:17 +0530

    wire it to main

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Build it first
 
 Login using `AWS_ACCESS_KEY_ID`, `AWS_SECRET_ACCESS_KEY`, `region` and `bucket-name` (need only AmazonS3ReadOnlyAccess)
 ```
-../bin/rclone aws-login
+./bin/rclone aws-login
 ```
 
 List the objects in the bucket
--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println(`usage:
-  storacha-rclone aws-login                 # save keys/region/bucket
-  storacha-rclone s3-ls [-prefix p/]        # list objects
-  storacha-rclone s3-get -key k [-out f]    # download object`)
+  storacha-rclone aws-login                 # save AWS keys/region/bucket
+  storacha-rclone s3-ls [-prefix p/]        # list S3 objects
+  storacha-rclone s3-get -key k [-out f]    # download from S3
+
+  storacha-rclone storacha-login            # save Storacha credentials
+  storacha-rclone storacha-put -file f      # upload file to Storacha`)
 		os.Exit(2)
 	}
 
@@ -23,6 +26,10 @@ func main() {
 		commands.S3List(os.Args[2:])
 	case "s3-get":
 		commands.S3Get(os.Args[2:])
+	case "storacha-login":
+		commands.StorachaLogin()
+	case "storacha-put":
+		commands.StorachaPut(os.Args[2:])
 	default:
 		fmt.Println("unknown command:", os.Args[1])
 		os.Exit(2)
