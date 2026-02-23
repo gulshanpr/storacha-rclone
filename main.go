d7c1c19e3433ed7a73b400f6c81a5aa05018ca66
amsorrytola <talhaansariitr@gmail.com>
2026-02-23 13:48:16 +0530

    update usage instructions in main.go to include storacha-get command

--- main.go
+++ main.go
@@ -10,14 +10,15 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println(`usage:
-  storacha-rclone aws-login                 # save AWS keys/region/bucket
-  storacha-rclone s3-ls [-prefix p/]        # list S3 objects
-  storacha-rclone s3-get -key k [-out f]    # download from S3
-  storacha-rclone s3-rm -key k              # delete from S3
+  storacha-rclone aws-login                 		# save AWS keys/region/bucket
+  storacha-rclone s3-ls [-prefix p/]        		# list S3 objects
+  storacha-rclone s3-get -key k [-out f]    		# download from S3
+  storacha-rclone s3-rm -key k              		# delete from S3
 
-  storacha-rclone storacha-login            # save Storacha credentials
-  storacha-rclone storacha-put -file f      # upload file to Storacha
-  storacha-rclone cp -s3-key k              # copy S3 object to Storacha`)
+  storacha-rclone storacha-login            		# save Storacha credentials
+  storacha-rclone storacha-put -file f      		# upload file to Storacha
+  storacha-rclone storacha-get -cid <cid> [-out f] 	# download from Storacha
+  storacha-rclone cp -s3-key k              		# copy S3 object to Storacha`)
 		os.Exit(2)
 	}
 
@@ -36,6 +37,8 @@ func main() {
 		commands.StorachaPut(os.Args[2:])
 	case "cp":
 		commands.Copy(os.Args[2:])
+	case "storacha-get":
+		commands.StorachaGet(os.Args[2:])
 	default:
 		fmt.Println("unknown command:", os.Args[1])
 		os.Exit(2)
